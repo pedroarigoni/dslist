047040554d9af61f07b7aafa9dc3f707fc7b30ed
Pedro Arigoni <pedroarigoni@gmail.com>
2023-11-13 23:15:42 -0300

    Game seed

--- src/main/java/com/pedroarigoni/dslist/entities/Game.java
+++ src/main/java/com/pedroarigoni/dslist/entities/Game.java
@@ -21,23 +21,31 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String shortDescription,
-			String longDescription) {
+	public Game(Long id, String title, String genre, String platforms, Double score, String imgUrl, String shortDescription,
+			String longDescription, Integer year) {
+		super();
 		this.id = id;
 		this.title = title;
-		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
+		this.year = year;
 	}
 
 	public Long getId() {
@@ -56,14 +64,6 @@ public class Game {
 		this.title = title;
 	}
 
-	public Integer getYear() {
-		return year;
-	}
-
-	public void setYear(Integer year) {
-		this.year = year;
-	}
-
 	public String getGenre() {
 		return genre;
 	}
@@ -72,12 +72,28 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getplatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setplatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public String getShortDescription() {
@@ -94,6 +110,14 @@ public class Game {
 
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
 	}
 
 	@Override
