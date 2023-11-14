34e9fb718adf8e3bd69ae33d324735680575f260
Pedro Arigoni <pedroarigoni@gmail.com>
2023-11-14 10:17:35 -0300

    Game repository, service, controller

---
+++ src/main/java/com/pedroarigoni/dslist/repositories/GameRepository.java
@@ -0,0 +1,10 @@
+package com.pedroarigoni.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedroarigoni.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+	
+}
