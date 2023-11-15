f0c0497bf9be7d9c156a6db81e642d99ed58bec9
pedroargioni <pedroargioni@gmail.com>
2023-11-14 23:36:56 -0300

    Consulta SQL, Projection, busca de games por lista

--- src/main/java/com/pedroarigoni/dslist/services/GameService.java
+++ src/main/java/com/pedroarigoni/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pedroarigoni.dslist.dto.GameDTO;
 import com.pedroarigoni.dslist.dto.GameMinDTO;
 import com.pedroarigoni.dslist.entities.Game;
+import com.pedroarigoni.dslist.projection.GameMinProjection;
 import com.pedroarigoni.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
